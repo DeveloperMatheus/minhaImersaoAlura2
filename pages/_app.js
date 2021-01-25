fee9c02327565f41db0349cb606ff612d6c4534e
developermatheus <matheus.developer@gmail.com>
2021-01-25 18:06:31 -0300

    Novos arquivos

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,16 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '../src/css/globalStyles';
+import db from '../db.json';
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`
+const MyGlobalStyle = GlobalStyle;
 
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+const theme = db.theme;
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>
+      <MyGlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,44 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import db from '../db.json';
+import QuizBackground from '../src/components/QuizBackground';
+import GitHubCorner from '../src/components/GitHubCorner';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import QuizLogo from '../src/components/QuizLogo';
 
 const Title = styled.h1`
   font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.secondary};
 `
 
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo className="mx-auto"/>
+        <Widget>
+          <Widget.Header>
+            Cabeçalho de um componente
+          </Widget.Header>
+          <Widget.Content>
+            <h1>The Legend of Zelda</h1>
+            <p>Teste ipsum dolor</p>
+          </Widget.Content>
+        </Widget>
+      </QuizContainer>
+      <Footer />
+      <GitHubCorner />
+    </QuizBackground>
+  )
 }
